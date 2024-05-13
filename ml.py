e30899bcbe113d3b4ad4617b4bbc621f2976cf51
unknown <ckirk0527@windstream.net>
2024-05-13 11:38:28 -0700

    first commit

---
+++ ml.py
@@ -0,0 +1,13 @@
+import sys
+import scipy
+
+import numpy
+import matplotlib
+import pandas as pd
+import sklearn
+from scipy import constants
+print(constants.liter)
+
+df = pd.read_csv('')
+print(df.to_string())
+  
